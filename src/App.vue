2ac7061462967c7aa22b55b61ebbe0f42a8ca79a
Douglas Saisho <douglas.saisho@mirumagency.com>
2019-04-13 18:02:28 -0700

    header basic functionality is built

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,28 @@
 <template>
-<v-app id="app" >
-  <v-container >
-    <v-layout row wrap justify-space-around>
-      <v-flex xs4 class="p-2">
-
-
-          <router-link class="mr-2" v-for="(value,key) in items" :key="key" :to=urls[key] >
-             <v-icon>{{icons[key]}}</v-icon>
-          </router-link>
-
-      </v-flex>
-
-      <v-flex xs4 >
-        <v-select class="ma-0 pa-0 vselect" v-on:change="onSelected" :items="items" label="Select Page"></v-select>
-      </v-flex>
-      <v-flex xs12>
-        <router-view />
-      </v-flex >
-    </v-layout>
-  </v-container>
-
-</v-app>
+  <v-app id="app">
+    <v-container  >
+      <Header />
+      <router-view />
+    </v-container>
+  </v-app>
 </template>
 <script>
+import Header from "@/components/header/index.vue"
 export default {
+  components:{
+        Header
+      },
   data() {
     return {
-      items: ["Home", "About", "Table Search", "Table Details", "Tabulator Demo", "Tabulator Demo Dynamic", "Upload Multiple", "Update Dataset"],
-      urls: ["./", "/about", "/table-search","/table-details","/tabulator", "tabulator-dynamic","upload-multiple","update-dataset"],
-      icons: ["home", "whatshot", "airplanemode_active", "view_list","build","accessible", "done_all","star"]
+      
+      urls: [
+        "./",
+        "/about"
+      ]
     };
   },
   methods: {
-    onSelected: function(e) {
-      const url = this.urls[this.items.indexOf(e)];
-      this.$router.push({ path: url });
-    }
+    
   }
 };
 </script>
---
+++ src/components/header/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-layout>
+    <v-card width="100%" class="d-flex">
+      <v-img src="./mirumlogo.jpg" aspect-ratio="1" class="grey lighten-2" max-width="150px"></v-img>
+
+      <v-flex align-center d-flex ml-3 xs6>
+        <div>
+          <h2>Data Driven Delivery</h2>
+          <span>view/edit project files for marketing-tag-delivery-uhc</span>
+        </div>
+      </v-flex>
+      <v-flex align-end d-flex red--text mb-3>
+        <a>Link for something 1</a>
+        <a>Link to do things</a>
+      </v-flex>
+    </v-card>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  mounted() {}
+};
+</script>
+
+<style scoped>
+a{
+    color:#d4005f
+}
+</style>
